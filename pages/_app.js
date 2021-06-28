aa5409695f26ac80b62bef5442f0e72571f9dbe8
Elias Carlson <carlson.elias@gmail.com>
2021-06-28 11:52:36 +0200

    initial setup and started on index site

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import '../styles/globals.sass'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Button, Drawer } from '@material-ui/core'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Birkabo Förvaltning</title>
+        <meta name="description" content="Hemsida för Birkabo Förvaltning" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        {/* <Image>Birkabo logga</Image> */}
+        <Drawer anchor={'right'} open={false}>
+          <h3>Test</h3>
+          <h3>Test</h3>
+          <h3>Test</h3>
+          <h3>Test</h3>
+          <h3>Test</h3>
+          <h3>Test</h3>
+        </Drawer>
+      </header>
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
 }
 
 export default MyApp
