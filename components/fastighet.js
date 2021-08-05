7153192bca9ab03468b55b69de55335194e15124
Elias Carlson <carlson.elias@gmail.com>
2021-08-05 12:01:04 +0200

    switched to a new carousel. implementation not finished

--- components/fastighet.js
+++ components/fastighet.js
@@ -1,18 +1,15 @@
 import styles from "../styles/modules/components/Fastighet.module.sass"
 import Image from "next/image"
-import Carousel from "react-bootstrap/Carousel"
-// import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
 
 export default function Fastighet(props) {
     var imgSrc = "/img/previews/" + props.estateShort + ".jpg"
 
     return (
         <div className={styles.container}>
-            {/* <Breadcrumb>
-                <Breadcrumb.Item href="/">Hem</Breadcrumb.Item>
-                <Breadcrumb.Item href="/fastigheter">Våra Fastigheter</Breadcrumb.Item>
-                <Breadcrumb.Item active>{props.estateName}</Breadcrumb.Item>
-            </Breadcrumb> */}
+
             <Image src={imgSrc} width={1200 / 2} height={800 / 2}></Image>
 
             <h1>{props.estateName}</h1>
@@ -20,22 +17,24 @@ export default function Fastighet(props) {
             <p><b>Byggnadsår: </b>{props.year}</p>
             <p><b>Område: </b>{props.area}</p>
 
-            <Carousel className={styles.carousel}>
-                <Carousel.Item>
-                    <Image src={imgSrc} width={1200 / 2} height={800 / 2}></Image>
-                </Carousel.Item>
+            <Carousel showIndicators={true} showArrows={true} showStatus={false} dynamicHeight={true} thumbWidth="200px">
+                {/*<Image src={imgSrc} width={1200 / 2} height={800 / 2}></Image>
 
-                <Carousel.Item>
-                    <Image src={imgSrc} width={1200 / 2} height={800 / 2}></Image>
-                </Carousel.Item>
+                <Image src={"/img/VR/20210528-IMG_6990.jpg"} width={1200 / 2} height={800 / 2}></Image>
 
-                <Carousel.Item>
-                    <Image src={imgSrc} width={1200 / 2} height={800 / 2}></Image>
-                </Carousel.Item>
-
+                <Image src={imgSrc} width={1200 / 2} height={800 / 2}></Image> */}
+                <div>
+                    <img alt="" src={imgSrc} />
+                </div>
+                <div>
+                    <img alt="" src={"/img/VR/20210528-IMG_6990.jpg"} />
+                </div>
+                <div>
+                    <img alt="" src={imgSrc} />
+                </div>
             </Carousel>
 
-            <iframe className={styles.embedMap} src={props.mapUrl} width="300" height="200" style={{border: 0}} allowFullScreen="" loading="lazy"></iframe>
+            <iframe className={styles.embedMap} src={props.mapUrl} width="300" height="200" style={{ border: 0 }} allowFullScreen="" loading="lazy"></iframe>
         </div>
     )
 }
