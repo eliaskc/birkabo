04ca267542001aab07f865f6e5fc8e0aa6bdae34
Elias Carlson <carlson.elias@gmail.com>
2023-03-23 15:20:23 +0100

    text-3xl instead of 4xl on estatecards

--- components/estateCard.js
+++ components/estateCard.js
@@ -13,7 +13,7 @@ export default function EstateCard(props) {
     return (
         <Link href={"fastigheter/" + props.estateShort}>
             <div className={bgImg + " h-full flex items-center justify-center bg-cover bg-black/40 bg-blend-darken lg:hover:bg-black/20 lg:cursor-pointer 2xl:h-96 2xl:rounded-[1.5rem] 2xl:drop-shadow-2xl"}>
-                <p className='text-white text-4xl font-semibold'>{props.estateName}</p>
+                <p className='text-white text-3xl font-semibold'>{props.estateName}</p>
             </div>
         </Link>
     )
