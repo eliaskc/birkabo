c1358f14a03f54b5948e3b0049594162036c9995
Elias Carlson <carlson.elias@gmail.com>
2023-05-15 18:41:08 +0200

    WIP

--- components/header.js
+++ components/header.js
@@ -2,13 +2,11 @@ import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import logo from '../public/birkabo_logo_no_bg.png';
-import { Navbar, MobileNav } from '@material-tailwind/react';
 import { IoClose, IoMenu } from 'react-icons/io5';
 
 export default function Header() {
     const [openNav, setOpenNav] = useState(false);
     const [scrolledPastHeader, setScrolledPastHeader] = useState(0);
-    const mobileMenuColor = 'white';
 
     useEffect(() => {
         const updateScrolledPastHeader = () => {
@@ -56,16 +54,13 @@ export default function Header() {
 
     return (
         <div
-            className={`sticky top-0 z-10 flex h-20 w-full justify-center px-8 transition-all duration-100 ease-in-out lg:p-0 ${
-                scrolledPastHeader && !openNav
+            className={`sticky z-10 w-full justify-center px-8 transition-all duration-100 ease-in-out lg:p-0 ${
+                scrolledPastHeader || !openNav
                     ? 'bg-white/25 shadow-md backdrop-blur-lg'
                     : ''
-            } ${openNav ? 'bg-white' : ''}`}
+            }`}
         >
-            <Navbar
-                className="container h-auto border-none bg-transparent p-0 text-black shadow-none"
-                fullWidth
-            >
+            <div className="container sticky border-none bg-transparent p-0 text-black shadow-none">
                 <div className="flex h-full items-center justify-between">
                     <Link
                         href="/"
@@ -87,13 +82,14 @@ export default function Header() {
                         />
                     )}
                 </div>
-                <MobileNav
-                    open={openNav}
-                    className="absolute left-0 h-auto bg-white py-8 shadow-lg"
+                <div
+                    className={`sticky left-0 w-screen py-8 shadow-lg ${
+                        !openNav ? 'hidden' : ''
+                    }`}
                 >
                     {navList}
-                </MobileNav>
-            </Navbar>
+                </div>
+            </div>
         </div>
     );
 }
